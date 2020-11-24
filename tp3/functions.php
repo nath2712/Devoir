1d141426749fd680060d31eab52b0090ddf975ee
nath2712 <61108573+nath2712@users.noreply.github.com>
2020-11-24 12:02:55 +0100

    Add files via upload

---
+++ tp3/functions.php
@@ -0,0 +1,32 @@
+
+<?php
+
+getHeader($startSession,$title)
+
+
+getFooter()
+
+
+
+
+?>
+<html lang="fr">
+
+
+		<head>
+			<meta charset = "UTF-8">
+			<title>Zx Td3</title>
+	
+		
+		</head>
+	<body>
+    <form method="post">
+				<label>Nom :</label>
+							<td><input type="text" name="nom" id ="nom" required placeholder="Entrez la raison pour laquelle vous voulez me contacter..."></td>
+
+			<input type="submit" value="Envoyer" id="envoyer" name="submitpost">
+    </form>
+	
+	
+		</body>
+</html>
